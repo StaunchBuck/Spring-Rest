0b07d9689940f02bb18b7a1a978032f596fdc810
fcadmin <fcadmin@192.168.1.4>
2021-09-07 13:37:43 +0530

    commit

--- restful-webservice/src/main/java/come/tech/spring/rest/restfulwebservice/RestfulWebserviceApplication.java
+++ restful-webservice/src/main/java/come/tech/spring/rest/restfulwebservice/RestfulWebserviceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RestfulWebserviceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RestfulWebserviceApplication.class, args);
+		SpringApplication.run(RestfulWebserviceApplication.class, args);//ddds
 	}
 
 }
